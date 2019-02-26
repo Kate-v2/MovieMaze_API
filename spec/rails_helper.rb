3a240552d8026688ba962596ccbe0469ba41725c
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-25 17:35:52 -0700

    Implement gems for tests in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,32 +30,44 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+require 'simplecov'
+SimpleCov.start "rails" do
+  add_filter'/bin/'
+  add_filter'/db/'
+  add_filter'/spec/'
+  add_filter'/config/'
+  add_filter'/app/channels/'
+  add_filter'app/jobs/'
+  add_filter'app/mailers/'
+  add_filter'app/helpers/'
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+require 'webmock/rspec'
+
+require 'capybara/rails'
+
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.fixture_path               = "#{::Rails.root}/spec/fixtures"   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.use_transactional_fixtures = true                              # If you're not using ActiveRecord, or you'd prefer not to run each of your examples within a transaction, remove the following line or assign false instead of true.
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
+                                                                        # RSpec Rails can automatically mix in different behaviours to your tests based on their file location, for example enabling you to call `get` and `post` in specs under `spec/controllers`.
+                                                                        # You can disable this behaviour by removing the line below, and instead explicitly tag your specs with their type, e.g.:
+                                                                        #     RSpec.describe UsersController, :type => :controller do  # ...  end
 
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.infer_spec_type_from_file_location!                            # The different available types are documented in the features, such as in  https://relishapp.com/rspec/rspec-rails/docs
+  config.filter_rails_from_backtrace!                                   # Filter lines from Rails gems in backtraces.
+  # config.filter_gems_from_backtrace("gem name")                       # arbitrary gems may also be filtered via:
+
+  config.include Capybara::DSL
+  Capybara.default_host = 'http://localhost:3000'
+  config.include FactoryBot::Syntax::Methods
 end
