296ed0819e33e17ba7c33f7034d82296af204989
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-26 20:19:04 -0700

    Presenters for Utelly Search Resutl

---
+++ app/presenters/movie_search_result.rb
@@ -0,0 +1,39 @@
+
+class MovieSearchResult
+
+  def initialize(data)
+    @data = data
+  end
+
+  def id
+    0
+  end
+
+  def picture
+    @data[:picture]
+  end
+
+  def title
+    @data[:name]
+  end
+
+  def streams
+    @data[:locations].map { |l|
+      {
+        service: l[:display_name],
+        url: l[:url]
+      }
+    }
+  end
+
+  # TO DO - maybe leave this to FE
+  # def valid_services
+  #   [
+  #     "Amazon Instant",
+  #     "Netflix",
+  #
+  #   ]
+  # end
+
+
+end
